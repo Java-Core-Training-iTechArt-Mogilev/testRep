f5954a79535f12591eb231a764ab546ae9887a8e
maxim <maxim.vorochaev@mail.ru>
2019-10-09 12:33:49 +0300

    l2

--- src/main/java/com/itechart/Main.java
+++ src/main/java/com/itechart/Main.java
@@ -1,9 +1,49 @@
 package com.itechart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
-        System.out.println(1 + 1);
+
+/*****************/
+//        List<int> list = new ArrayList<>();
+//        List<Integer> list1 = new ArrayList<>();
+/*****************/
+//        byte c, a, t;
+//        c = 23;
+//        a = 123123;
+/*****************/
+//        long night = 101L;
+//        System.out.println(night);
+/*****************/
+//        float tugrik = 11.6F;
+//        float tugrik1 = 11.6;
+/*****************/
+//        double sin = Math.sin(23.2);
+//        double sqrt = Math.sqrt(23.2);
+/*****************/
+//        double positive_infinity = 12.0 / 0;
+//        System.out.println(positive_infinity);
+/*****************/
+//        char ch1, ch2, ch3;
+/*****************/
+//        ch1 = 67; // код символа
+//        ch2 = 'a'; // сам символ
+//        ch3 = 116; // код символа
+/*****************/
+//        System.out.println("" + ch1 + ch2 + ch3);
+/*****************/
+//        char myChar = '\u00A9'; //шестнадцатеричное представление кода
+//        System.out.println(myChar);
+/*****************/
+//        boolean check;
+//        check = true;
+//
+//        System.out.println(check == false);
+/*****************/
+
     }
 }
